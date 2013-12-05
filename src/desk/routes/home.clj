81c1922472715e9856e1034ba48ef625b2917e82
Roman <felixcon@gmail.com>
2013-12-06 00:27:21 +0400

    initial commit

---
+++ src/desk/routes/home.clj
@@ -0,0 +1,15 @@
+(ns desk.routes.home
+  (:use compojure.core)
+  (:require [desk.views.layout :as layout]
+            [desk.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
